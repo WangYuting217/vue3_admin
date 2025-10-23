1f667822dc9cf2adf4220cc5af74caba0db68cbe
wangyuting <1540619804@qq.com>
2025-10-23 16:11:43 +0800

    SPU静态搭建

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        <h2>spu管理</h2>
+        <Category :scene="scene"></Category>
+        <el-card style="margin: 10px 0px;">
+            <el-button type="primary" size="default" icon="Plus">添加SPU</el-button>
+            <el-table style="margin: 10px 0px;" border>
+                <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+                <el-table-column label="SPU名称"></el-table-column>
+                <el-table-column label="SPU描述"></el-table-column>
+                <el-table-column label="操作">
+                    <el-button type="primary" size="small" icon="Plus"></el-button>
+                    <el-button type="primary" size="small" icon="Edite"></el-button>
+                    <el-button type="primary" size="small" icon="InfoFilled"></el-button>
+                    <el-button type="danger" size="small" icon="Delete"></el-button>
+                </el-table-column>
+            </el-table>
+            <!--分页器-->
+            <el-pagination v-model:current-page="pageNo" :page-sizes="pageSize" :page-size="[3,5,7,9]" :background="true"
+                layout="prev, pager, next, jumper,->,sizes,total " :total="400"/>
+        </el-card>
     </div>
 </template>
 
 <script setup lang='ts'>
-
+import { ref } from 'vue';
+let scene = ref<number>(0)
+//分页器默认页码
+let pageNo = ref<number>(1)
+//每一页展示几条数据
+let pageSize=ref<number>(3)
 </script>
 
 <style scoped></style>
